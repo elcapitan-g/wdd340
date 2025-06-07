5b42ba231ce8c07ac3179d9c71d148c65a5d364a
Your Nameelcapitan-g <gnewell89@gmail.com>
2025-06-07 01:53:23 -1000

    please im so tired

--- controllers/invController.js
+++ controllers/invController.js
@@ -12,11 +12,12 @@ invCont.buildByClassificationId = async function (req, res, next) {
     try {
         const data = await invModel.getInventoryByClassificationId(classification_id);
         const grid = await utilities.buildByClassificationGrid(data);
-        const className = data[0].classification_name; 
+        const className = data[0].classification_name; // Assuming the classification name is in the first record
 
         baseController.renderView(req, res, "./inventory/classification", {
             title: className + " Vehicles",
             grid,
+            classificationId: classification_id, // Pass classificationId to view
         });
     } catch (error) {
         console.error("Error in buildByClassificationId:", error);
@@ -27,21 +28,30 @@ invCont.buildByClassificationId = async function (req, res, next) {
 // Display details of a specific inventory item
 invCont.buildByInventoryId = async function (req, res, next) {
     const inv_id = req.params.inv_id;
+    const classificationId = req.query.classificationId;  // Get classificationId from query parameters
+
     try {
         const data = await invModel.getInventoryById(inv_id);
-        const grid = await utilities.buildInventoryDetail(data);
+        if (!data || data.length === 0) {
+            throw new Error("Inventory item not found");
+        }
 
-        baseController.renderView(req, res, "./inventory/inventoryDetail", {
-            title: `${data[0].inv_year} ${data[0].inv_make} ${data[0].inv_model}`,
-            inv_year: data[0].inv_year,
-            inv_make: data[0].inv_make,
-            inv_model: data[0].inv_model,
-            inv_description: data[0].inv_description,
-            inv_image: data[0].inv_image,
-            inv_price: data[0].inv_price,
-            inv_miles: data[0].inv_miles,
-            inv_color: data[0].inv_color,
+        const grid = await utilities.buildInventoryDetail(data); // Process grid, if necessary
+        const item = data[0]; // The actual inventory item
+
+        // Pass classificationId along with other data to the view
+        baseController.renderView(req, res, "inventory/inventorydetail", {
+            title: `${item.inv_year} ${item.inv_make} ${item.inv_model}`,
+            inv_year: item.inv_year,
+            inv_make: item.inv_make,
+            inv_model: item.inv_model,
+            inv_description: item.inv_description,
+            inv_image: item.inv_image,
+            inv_price: item.inv_price,
+            inv_miles: item.inv_miles,
+            inv_color: item.inv_color,
             grid,
+            classificationId,  // Pass classificationId to the view
         });
     } catch (error) {
         console.error("Error in buildByInventoryId:", error);
