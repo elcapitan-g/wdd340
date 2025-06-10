eaa143fc9de02aabb099bfe577fc05f2b3b0500f
Your Nameelcapitan-g <gnewell89@gmail.com>
2025-06-09 23:52:36 -1000

    uhhhhh mesa so tired

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,46 +1,68 @@
 "use strict";
 
-// Get a list of items in inventory based on the classification_id
-let classificationList = document.querySelector("#classificationList");
-classificationList.addEventListener("change", listChange);
-listChange(); // Do it once on load.
 
+const classificationList = document.querySelector("#classificationList");
+classificationList.addEventListener("change", listChange);
+listChange(); 
 
 function listChange() {
-    let classification_id = classificationList.value;
-    let classIdURL = "/inv/getInventory/" + classification_id;
-    fetch(classIdURL)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-            throw Error("Network response was not OK");
-        })
-        .then(function (data) {
-            buildInventoryList(data);
-        })
-        .catch(function (error) {
-            console.log("There was a problem: ", error.message);
-            document.getElementById("inventoryDisplay").innerHTML = "No vehicles in this category"; // Wanted something here
-        });
+  const classification_id = classificationList.value;
+
+  
+  if (!classification_id) {
+    document.getElementById("inventoryDisplay").innerHTML = "";
+    return;
+  }
+
+  const classIdURL = "/inv/getInventory/" + classification_id;
+
+  fetch(classIdURL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network response was not OK");
+    })
+    .then((data) => {
+      buildInventoryList(data);
+    })
+    .catch((error) => {
+      console.log("There was a problem: ", error.message);
+      document.getElementById("inventoryDisplay").innerHTML =
+        "<p class='notice'>No vehicles in this category.</p>";
+    });
 }
 
-// Build inventory items into HTML table components and inject into DOM
+
 function buildInventoryList(data) {
-    let inventoryDisplay = document.getElementById("inventoryDisplay");
-    // Set up the table labels
-    let dataTable = "<thead>";
-    dataTable += "<tr><th>Vehicle Name</th><td>&nbsp;</td><td>&nbsp;</td></tr>";
-    dataTable += "</thead>";
-    // Set up the table body
-    dataTable += "<tbody>";
-    // Iterate over all vehicles in the array and put each in a row
-    data.forEach(function (element) {
-        dataTable += `<tr><td>${element.inv_make} ${element.inv_model}</td>`;
-        dataTable += `<td><a href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>`;
-        dataTable += `<td><a href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td></tr>`;
-    });
-    dataTable += "</tbody>";
-    // Display the contents in the Inventory Management view
-    inventoryDisplay.innerHTML = dataTable;
+  const inventoryDisplay = document.getElementById("inventoryDisplay");
+
+  if (!data.length) {
+    inventoryDisplay.innerHTML = "<p class='notice'>No vehicles found.</p>";
+    return;
+  }
+
+  let dataTable = `
+    <thead>
+      <tr>
+        <th>Vehicle Name</th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+  `;
+
+  data.forEach((element) => {
+    dataTable += `
+      <tr>
+        <td>${element.inv_make} ${element.inv_model}</td>
+        <td><a href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>
+        <td><a href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td>
+      </tr>
+    `;
+  });
+
+  dataTable += "</tbody>";
+  inventoryDisplay.innerHTML = dataTable;
 }
