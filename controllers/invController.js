2b1bb33178059781a9165643e12db8e9ec245e7c
Your Nameelcapitan-g <gnewell89@gmail.com>
2025-06-09 16:49:39 -1000

    tried?

--- controllers/invController.js
+++ controllers/invController.js
@@ -51,7 +51,7 @@ invCont.buildByInventoryId = async function (req, res, next) {
  */
 invCont.buildManagementView = async function (req, res, next) {
   let nav = await utilities.getNav();
-  let classificationSelect = await utilities.buildClassificationList();
+  const classificationSelect = await utilities.buildClassificationList();
   res.render("inventory/management", {
     title: "Vehicle Management",
     errors: null,
