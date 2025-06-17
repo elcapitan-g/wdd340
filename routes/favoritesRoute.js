1addb1c8ba2da61d27ee0f4af07e9c86990571d0
Your Nameelcapitan-g <gnewell89@gmail.com>
2025-06-17 12:50:47 -1000

    fav final D:

--- routes/favoritesRoute.js
+++ routes/favoritesRoute.js
@@ -3,7 +3,6 @@ const router = new express.Router();
 const favoritesController = require("../controllers/favoritesController");
 const utilities = require("../utilities");
 
-// Middleware to check if user logged in
 function requireLogin(req, res, next) {
   if (!res.locals.loggedin) {
     req.flash("error", "You must be logged in to view this page.");
@@ -12,13 +11,10 @@ function requireLogin(req, res, next) {
   next();
 }
 
-// View favorites list
 router.get("/", requireLogin, utilities.handleErrors(favoritesController.showFavorites));
 
-// Add to favorites
 router.post("/add", requireLogin, utilities.handleErrors(favoritesController.addFavorite));
 
-// Remove from favorites
 router.post("/remove", requireLogin, utilities.handleErrors(favoritesController.removeFavorite));
 
 module.exports = router;
