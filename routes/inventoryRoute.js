80cf44ddd72f80cb5171e971fdf57e1225d6bca2
elcapitan-g <167657494+elcapitan-g@users.noreply.github.com>
2025-06-16 19:49:46 -1000

    Add files via upload

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,16 +3,22 @@ const router = new express.Router();
 const invController = require("../controllers/invController");
 const utilities = require("../utilities");
 const invValidate = require("../utilities/inventory-validation");
+const checkAccountType = require("../middleware/checkAccountType");
 
-router.get("/",
+// Management view (protected)
+router.get(
+  "/",
+  checkAccountType,
   utilities.handleErrors(invController.buildManagementView)
 );
 
+// Public JSON fetch
 router.get(
   "/getInventory/:classification_id",
   utilities.handleErrors(invController.getInventoryJSON)
 );
 
+// Public routes (visible to all)
 router.get(
   "/type/:classificationId",
   utilities.handleErrors(invController.buildByClassificationId)
@@ -23,14 +29,16 @@ router.get(
   utilities.handleErrors(invController.buildByInventoryId)
 );
 
-
+// Protected routes
 router.get(
   "/add-classification",
+  checkAccountType,
   utilities.handleErrors(invController.buildAddClassification)
 );
 
 router.post(
   "/add-classification",
+  checkAccountType,
   invValidate.classificationRules(),
   invValidate.checkClassificationData,
   utilities.handleErrors(invController.addClassification)
@@ -38,11 +46,13 @@ router.post(
 
 router.get(
   "/add-inventory",
+  checkAccountType,
   utilities.handleErrors(invController.buildAddInventory)
 );
 
 router.post(
   "/add-inventory",
+  checkAccountType,
   invValidate.inventoryRules(),
   invValidate.checkInventoryData,
   utilities.handleErrors(invController.addInventory)
@@ -50,11 +60,13 @@ router.post(
 
 router.get(
   "/edit/:inventoryId",
+  checkAccountType,
   utilities.handleErrors(invController.buildEditInventory)
 );
 
 router.post(
   "/update",
+  checkAccountType,
   invValidate.inventoryRules(),
   invValidate.checkUpdateData,
   utilities.handleErrors(invController.updateInventory)
@@ -62,11 +74,13 @@ router.post(
 
 router.get(
   "/delete/:inventoryId",
+  checkAccountType,
   utilities.handleErrors(invController.buildDeleteInventory)
 );
 
 router.post(
   "/delete/",
+  checkAccountType,
   utilities.handleErrors(invController.deleteInventory)
 );
 
