4659142ad49cae03464c4a8f2f52ffc4335262a9
elcapitan-g <167657494+elcapitan-g@users.noreply.github.com>
2025-06-16 15:05:37 -1000

    Add files via upload

---
+++ routes/static.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const utilities = require("../utilities")
+
+router.use(express.static("public"));
+router.use("/css", utilities.handleErrors(express.static(__dirname + "public/css")));
+router.use("/js", utilities.handleErrors(express.static(__dirname + "public/js")));
+router.use("/images", utilities.handleErrors(express.static(__dirname + "public/images")));
+
+module.exports = router;
